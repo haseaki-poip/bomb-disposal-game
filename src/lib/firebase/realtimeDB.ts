cbfad1d308b63f0386df240b73bef6656d925fd4
Hasegawa-Akito <h.akito1999@gmail.com>
2023-07-18 15:59:06 +0900

    room作成処理実装

--- src/lib/firebase/realtimeDB.ts
+++ src/lib/firebase/realtimeDB.ts
@@ -1,12 +1,52 @@
 import { ref, update, set, push, get, child } from "firebase/database";
 import { realtimeDB } from "./firebase";
 
+// シークレットIDの生成
+const generateSecretId = (): string => {
+  const idLength = 6;
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let secretId = "";
+
+  for (let i = 0; i < idLength; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    secretId += characters.charAt(randomIndex);
+  }
+
+  return secretId;
+};
+
+// ルームの作成
+export const createRoom = async () => {
+  const secretId = generateSecretId();
+
+  const postListRefRooms = ref(realtimeDB, "/");
+  const newPostRefRooms = push(postListRefRooms);
+  await set(newPostRefRooms, {
+    rooms: { secret_id: secretId, status: "waiting" },
+    members: [
+      {
+        user_name: "user1",
+        user_type: "admin",
+      },
+    ],
+  });
+
+  const roomId = newPostRefRooms.key;
+  if (!roomId) {
+    return null;
+  }
+
+  return {
+    roomId: roomId,
+    secretId: secretId,
+  };
+};
+
 // idからメンバー情報取得
 export const getMembersInfo = async (roomId: string) => {
   const dbRef = ref(realtimeDB);
-  const snapshot = await get(child(dbRef, `members/${roomId}`));
-  const { members_info: membesInfo } = (await snapshot.val()) ?? {
-    members_info: null,
-  };
-  return membesInfo;
+  const snapshot = await get(child(dbRef, `${roomId}/members`));
+  const membesInfoList = (await snapshot.val()) ?? null;
+  return membesInfoList;
 };
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,23 @@
-import { getMembersInfo } from "@/lib/firebase/realtimeDB";
+import { createRoom, getMembersInfo } from "@/lib/firebase/realtimeDB";
 
 const Login = () => {
   const confirm = async () => {
     await getMembersInfo("12345");
   };
-  return <div onClick={() => confirm()}>Login Page</div>;
+  const createRoomProcess = async () => {
+    const response = await createRoom();
+    if (!response) {
+      return;
+    }
+    const roomId = response.roomId;
+    const selectId = response.secretId;
+  };
+  return (
+    <div>
+      <div onClick={() => confirm()}>confirm</div>
+      <div onClick={() => createRoomProcess()}>create room</div>
+    </div>
+  );
 };
 
 export default Login;
