94f9a3e1f94866d8cb63fd57fceb26fd91472a27
Hasegawa-Akito <h.akito1999@gmail.com>
2023-07-23 14:17:24 +0900

    UI切り替えコンポーネント実装

--- src/pages/game/[roomId].tsx
+++ src/pages/game/[roomId].tsx
@@ -1,13 +1,15 @@
+import SwitchComponents from "@/components/Game/SwitchComponents";
 import useConfirmEligibility from "@/components/hooks/useConfirmEligibility";
 import useRealTimeMembers from "@/components/hooks/useRealTimeMembers";
 import { useRouter } from "next/router";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 const Game = () => {
   const router = useRouter();
   const { roomId } = router.query;
   const { membersInfoList } = useRealTimeMembers(roomId);
   const { userId } = useConfirmEligibility(roomId, "inGame");
+  const [isProfilePage, setIsProfilePage] = useState(true);
 
   const isLogin = useMemo(() => {
     return userId != null && membersInfoList[userId]?.user_type;
@@ -18,7 +20,15 @@ const Game = () => {
   }
 
   return (
-    <div className="w-full h-full min-h-screen bg-login-main-color pt-16"></div>
+    <>
+      <div className="w-full h-full min-h-screen bg-game-main-color pt-16"></div>
+      <div className="fixed bottom-0 left-0 z-50 w-full shadow">
+        <SwitchComponents
+          isProfilePage={isProfilePage}
+          setIsProfilePage={setIsProfilePage}
+        />
+      </div>
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,7 @@ module.exports = {
         "game-red": "#8B0000",
         "game-blue": "#1E90FF",
         "game-gray": "#808080",
+        "game-main-color": "#F6F6F6",
         "login-main-color": "#D0ADAD",
       },
     },
